31450cb0095a61e99489d0a7249a57e236945546
Hina19911 <Hina.Hina@triosstudent.com>
2025-02-07 10:36:02 -0500

    Initial Commit

---
+++ iOSApp2Tests/iOSApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp2Tests.swift
+//  iOSApp2Tests
+//
+//  Created by Maryam Hina on 2025-02-07.
+//
+
+import Testing
+@testable import iOSApp2
+
+struct iOSApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
